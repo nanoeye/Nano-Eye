3a4cd0de4d691aff1b14a9f44fd87b5b8dd134ba
Nano Eye <nanoeye@users.noreply.github.com>
2017-02-15 13:19:08 +0600

    Add files via upload

---
+++ system/themes/aOffice/configs.php
@@ -0,0 +1,13 @@
+<?php
+
+function get_layout_positions(){
+
+    return array(
+        'header' => array(),
+        'top' => array(),
+        'titlebar' => array(),
+        'left' => array(),
+        'right' => array(),
+        'footer' => array()
+    );
+}
